ffaa99788411aeac1bd083ada036ba9cf06f41ec
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-07-19 14:58:05 +0100

    add template

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+ };
