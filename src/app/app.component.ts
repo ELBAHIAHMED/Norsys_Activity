8ef61fe26ffa406b516aa1b49b047eda458e3c20
Ahmed EL BAHI <ahmed.elabhi99@gmail.com>
2022-08-03 16:41:24 +0100

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AdminService } from './services/admin.service';
-
+// @ts-ignore
+import AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'norsys-activity';
+
+
+
+  ngOnInit(): void {
+    AOS.init();
+
+  }
 }
